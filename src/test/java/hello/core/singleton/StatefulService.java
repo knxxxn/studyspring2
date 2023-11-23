c5815504438bf5992cb5dae92debcfec468ba29b
knxxxn <20211308@sungshin.ac.kr>
2023-11-23 22:26:58 +0900

    싱글톤

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,21 @@
+package hello.core.singleton;
+
+//상태를 유지할 경우 발생하는 문제점 예시
+public class StatefulService {
+    private int price; //상태를 유지하는 필드
+    public void order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        this.price = price; //여기가 문제!
+    }
+    public int getPrice() {
+        return price;
+    }
+
+    //무상태로 해보자면
+    /*
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+     */
+
+}
